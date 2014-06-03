af12b32ab16203e4f2bb6dd21d2b1d459f041fa3
Jamil Appa <jamil.appa@zenotech.com>
2014-06-03 16:59:52 +0100

    Added to read me

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ SysScribe
 =========
 
 Records system setup as a python dictionary
+
+Note only tested in RedHat Linux
+
+Installation:
+
+python setup.py install
+
+Example:
+
+from sysscribe import system
+print system.system_dict()
--- sysscribe/__init__.py
+++ sysscribe/__init__.py
@@ -78,7 +78,7 @@ def size(device):
     nr_sectors = open(device+'/size').read().rstrip('\n')
     try:
         sect_size = open(device+'/queue/hw_sector_size').read().rstrip('\n')
-    except:
+    except: # For rh5.4 need the following
         sect_size = open(device+'/queue/max_sectors_kb').read().rstrip('\n')
 
     # The sect_size is in bytes, so we convert it to GiB and then send it back
