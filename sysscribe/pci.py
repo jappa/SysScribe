ce9b86c88aa9dda8481f4cd2b298d1f8388dbe0f
Jamil Appa <jamil.appa@zenotech.com>
2014-06-03 16:53:07 +0100

    bug fix

--- sysscribe/pci.py
+++ sysscribe/pci.py
@@ -3,12 +3,12 @@ from sysscribe import pciinfo
 def pci_list():
     pciinf = pciinfo()
     pci_list=[]
-    for pci in pci_list.keys():
+    for pci in pciinf.keys():
         pci_list.append(pciinf[pci])
     return pci_list
         
 def print_list():
     pciinf = pciinfo()
-    for pci in pci_list.keys():
+    for pci in pciinf.keys():
         print(pciinf[pci])
         
