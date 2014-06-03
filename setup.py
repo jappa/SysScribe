421ca508dde644cf9a3cdbc6eaeda154bd11669a
Jamil Appa <jamil.appa@zenotech.com>
2014-06-03 16:48:06 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='SysScribe',
+    version='0.1',
+    packages=['sysscribe',],
+    license='BSD license',
+    long_description=open('README.txt').read(),
+)
